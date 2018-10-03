f4b712133166e5733b597c5636a6b2b68af33f0f
Jessica Benson <jessbnsn@gmail.com>
2018-10-03 13:24:35 +0200

    More animation & smooth scrolling

---
+++ src/js/inject-animation-on-reveal.js
@@ -0,0 +1,18 @@
+$(window).scroll(function() {
+    if (checkVisible($("#reveal"))) {
+        $('#reveal').addClass("add-animation");
+    } else {
+        $('#reveal').removeClass("add-animation");
+    }
+});
+
+function checkVisible( elm, eval ) {
+    eval = eval || "visible";
+    var vpH = $(window).height(), // Viewport Height
+        st = $(window).scrollTop(), // Scroll Top
+        y = $(elm).offset().top,
+        elementHeight = $(elm).height();
+    
+    if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+    if (eval == "above") return ((y < (vpH + st)));
+}
